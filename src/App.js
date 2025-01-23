444aba7f6d22e29734519d87865dd1b202a67c66
Olatunde Ebenezer Obaloluwa <olatundeobaloluwa@gmail.com>
2025-01-23 15:24:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { motion, AnimatePresence } from "framer-motion";
 
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5000/", // Flask backend URL
+  baseURL: "https://palindrome-backend-1.onrender.com", // Flask backend URL
 });
 
 function App() {
